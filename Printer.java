bc0b60f194a405f37f045bdc2f45f77541f73515
alekseyblinov <65337939+alekseyblinov@users.noreply.github.com>
2020-10-15 16:11:34 +0300

    Исправления в задании

--- Printer.java
+++ Printer.java
@@ -5,23 +5,20 @@ public class Printer {
     String documentPaperSize;
     int paperCount;
 
-    Printer (String documentType, String documentPaperSize, int paperCount) {
-        this.documentType = documentType;
-        this.documentPaperSize = documentPaperSize;
-        this.paperCount = paperCount;
-
-        System.out.println("Тип документа: " + documentType);
-        System.out.println("Размер бумаги для печати: " + documentPaperSize);
-        System.out.println("Количество копий: " + paperCount);
-    }
+    static int count = 0;
 }
+class Photo {
+    String color;
+    int size;
+    String photoType;
 
-class PrinterTest {
-    public static void main(String[] args) {
-        Printer P1 = new Printer("Photo", "10x15", 4);
-        System.out.println();
-        Printer P2 = new Printer("Report", "A4", 20);
-        System.out.println();
-        Printer P3 = new Printer("Scheme", "A2",1);
+    public Photo(String color, int size, String photoType) {
+        this.color = color;
+        this.size = size;
+        this.photoType = photoType; // фотография на визу/обычное фото/фото на паспорт
+
+        System.out.println("Вы печатаете фотографию");
+        System.out.println("Фотография цветная/чёрно белая: " + color);
+        System.out.println("Фотография на: " + photoType);
     }
 }
